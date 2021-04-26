6d827eba75b4b37b60c25c50be76e2c033ce17e7
nikhilshankar96 <nikhil-shankar@outlook.com>
2021-04-26 16:15:26 -0700

    init

--- Scrabble/ContentView.swift
+++ Scrabble/ContentView.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+let gridSize = 15;
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 1){
+            ForEach(0...(gridSize-1), id: \.self){ i in
+                HStack(spacing: 1){
+                ForEach(0...(gridSize-1), id: \.self){ j in
+                    Button(action: {
+                        //action
+                        print("loc(\(i),\(j))")
+                    }, label: {
+                        Text("O")
+                    })
+                    .buttonStyle(CellStyle())
+                }
+                }
+            }
+        }
+        .background(Color.black)
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CellStyle: ButtonStyle {
+    let cellSize : CGFloat = 19.0;
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .font(.system(size: cellSize, design: .monospaced))
+            .frame(width: cellSize, height: cellSize, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .padding(2)
+            .foregroundColor(Color.black)
+            .background(Color.white)
+//            .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
     }
 }
