d82e2b9677cd02747902fd177a7e0fb9f97b8884
nikhilshankar96 <nikhil-shankar@outlook.com>
2021-04-26 15:45:01 -0700

    Initial Commit

---
+++ Scrabble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrabble
+//
+//  Created by Nikhil Shankar on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scrabble/ScrabbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrabbleApp.swift
+//  Scrabble
+//
+//  Created by Nikhil Shankar on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrabbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
